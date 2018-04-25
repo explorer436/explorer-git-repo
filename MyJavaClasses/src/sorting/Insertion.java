44b3ac785be003c02521dafbbb406bc3f1e6d984
explorer436 <harshavardhane@gmail.com>
2018-04-25 17:15:05 -0400

    updates

--- MyJavaClasses/src/sorting/Insertion.java
+++ MyJavaClasses/src/sorting/Insertion.java
@@ -1,12 +1,25 @@
 package sorting;
 
+/*
+ * Example:   The following table shows the sorting steps for sorting the sequence 5 7 0 3 4 2 6 1. On the left side, in green, is the already sorted part of the sequence. On the far right is the number of positions in parentheses around which the inserted element has moved to the left.
+
+5	7	0	3	4	2	6	1	 	(0)
+5	7	0	3	4	2	6	1	 	(0)
+0	5	7	3	4	2	6	1	 	(2)
+0	3	5	7	4	2	6	1	 	(2)
+0	3	4	5	7	2	6	1	 	(2)
+0	2	3	4	5	7	6	1	 	(4)
+0	2	3	4	5	6	7	1	 	(1)
+0	1	2	3	4	5	6	7	 	(6)
+
+ */
 public class Insertion
 {
 
 	public static void sort(Comparable[] a)
 	{
-		int n = a.length;
-		for (int i = 1; i < n; i++)
+		int length = a.length;
+		for (int i = 1; i < length; i++)
 		{
 			for (int j = i; j > 0; j--)
 			{
@@ -34,12 +47,21 @@ public class Insertion
 	// them out in sorted order
 	public static void main(String[] args)
 	{
-		String[] a = new String[] { "ghi", "abc", "def" };
+		String[] strArray = new String[] { "ghi", "abc", "def" };
 
-		sort(a);
-		for (int i = 0; i < a.length; i++)
+		sort(strArray);
+		for (int i = 0; i < strArray.length; i++)
 		{
-			System.out.println(a[i] + " ");
+			System.out.println(strArray[i] + " ");
+		}
+		System.out.println();
+		
+		Integer[] intArray = new Integer[] { 5, 7, 0, 3, 4, 2, 6, 1 };
+
+		sort(intArray);
+		for (int i = 0; i < intArray.length; i++)
+		{
+			System.out.println(intArray[i] + " ");
 		}
 		System.out.println();
 	}
